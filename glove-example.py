ac5b50b5b96a767959879c5512bc6583f2f5a806
Jordan Taylor <43645687+jordan-wei-taylor@users.noreply.github.com>
2021-02-09 23:36:47 +0000

    Update glove-example.py

--- glove-example.py
+++ glove-example.py
@@ -13,18 +13,8 @@ if file not in os.listdir():
     raise FileNotFoundError(f'Please download "{file}" from https://www.kaggle.com/c/word2vec-nlp-tutorial to run this script')
     
 # Download data from https://www.kaggle.com/c/word2vec-nlp-tutorial
-data = pd.read_csv(file, header = 0, delimiter = '\t')
-
-np.random.seed(2021)
-
-n     = len(data)
-idx   = np.random.permutation(n)
-split = int(0.8 * n)
-train = idx[:split]
-test  = idx[split:]
-
-X_train, X_test = data['review'   ].values[train], data['review'   ].values[test]
-y_train, y_test = data['sentiment'].values[train], data['sentiment'].values[test]
+data    = pd.read_csv(file, header = 0, delimiter = '\t')
+reviews = data['review'].values
 
 del data; gc.collect()
 
@@ -32,13 +22,13 @@ preprocessor = get_preprocessor(stem = True, stop = True, min_length = 3)
 folder       = 'dump'
 os.makedirs(folder, exist_ok = True)
 
-file       = 'setup.npz'
+file = 'setup.npz'
 if file in os.listdir(folder):
     logger(f'found saved {file}')
     glove  = Glove(None   , preprocessor, random_state = 2021)
     glove.load(f'{folder}/{file}')
 else:
-    glove  = Glove(X_train, preprocessor, random_state = 2021)
+    glove  = Glove(reviews, preprocessor, random_state = 2021)
     glove.dump_co_occurance(f'{folder}/{file}')
     logger(f'saved {file}')
 
@@ -49,4 +39,3 @@ for dim in [2, 10, 50, 100, 200, 300, 400, 500, 600]:
     glove.dump_vectors(f'{folder}/glove-{dim}.npz')
     logger(f'saved {filename}')
     print()
-    
